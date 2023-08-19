81061ae0a90b8bc158d738336f856a9114d9623c
sebas97castrillon <sebas.2561@hotmail.com>
2023-08-19 10:59:45 -0500

    Final2

--- src/package2/Main.java
+++ src/package2/Main.java
@@ -5,7 +5,7 @@ import src.builder.Empleado;
 public class Main {
 
     public static void main(String[] args) {
-        Empleado empleado = Empleado.builder().build();
+        Empleado empleado = Empleado.builder().nombre("Osar").build();
         System.out.println(empleado);
     }
 }
